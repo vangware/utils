703caaec9f975803170beb372ace7da7670a38b2
Luke Shiru <hi@luke.sh>
2020-07-12 02:42:33 -0700

    :bug: fix `numberToDecimalTuple` parsing error with numbers that had exponent. :white_check_mark: add tests for those scenarios.

--- src/types/DecimalTuple.ts
+++ src/types/DecimalTuple.ts
@@ -1,6 +1,8 @@
+import { Tuple } from "./Tuple";
+
 /**
  * 2-Tuple to represent decimals [coefficient, exponent]
  */
-export type DecimalTuple = readonly [number, number];
+export type DecimalTuple = Tuple<number>;
 
 export default DecimalTuple;
