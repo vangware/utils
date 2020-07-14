8d862ee451ac9b57d2e6056bd85372a72fd6ecdf
Lucas Ciruzzi <hi@lshi.ru>
2020-07-13 20:39:53 -0700

    :arrow_up: dependency upgrade. :bug: fix order of `decimalTupleAdd` and `numberAdd` (minor because is an addition).

--- src/number/numberAdd.ts
+++ src/number/numberAdd.ts
@@ -12,18 +12,18 @@ import { numberToDecimalTuple } from "./numberToDecimalTuple";
  *
  * addPoint1(0.2); // 0.3
  * ```
- * @param addend1 First addend of the addition.
+ * @param addend2 Second addend of the addition.
  * @returns Curried function with `addend1` set in context.
  */
-export const numberAdd = (addend1: number) =>
+export const numberAdd = (addend2: number) =>
 	/**
-	 * @param addend2 Second addend of the addition.
+	 * @param addend1 First addend of the addition.
 	 * @returns The result of `addend1` + `addend2`.
 	 */
-	(addend2: number) =>
+	(addend1: number) =>
 		decimalTupleToNumber(
-			decimalTupleAdd(numberToDecimalTuple(addend1))(
-				numberToDecimalTuple(addend2)
+			decimalTupleAdd(numberToDecimalTuple(addend2))(
+				numberToDecimalTuple(addend1)
 			)
 		);
 
