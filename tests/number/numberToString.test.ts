42e7a2f044152ea295dbe2f66a4c42faca96d178
Lucas Ciruzzi <hi@lshi.ru>
2020-11-27 00:01:02 -0800

    :arrow_up: dependency upgrade. :building_construction: update `equalValues` to use `Object.is`. :building_construction: update `equal` utils to use `equalValues` when comparing values directly. :building_construction: update `numberToDecimalTuple` internal code. :sparkles: add new `numberToString` util. :sparkles: add new `numberToStringDecimal` util. :sparkles: add new `numberToStringHexadecimal` util. :sparkles: add new `objectGetProperty` util. :sparkles: add new `objectSetProperty` util. :sparkles: add new `Radix` type. :bug: fix some root imports. :white_check_mark: add tests for new utils.

---
+++ tests/number/numberToString.test.ts
@@ -0,0 +1,23 @@
+import { suite } from "@vangware/test";
+import { numberToString } from "../../src/number/numberToString";
+
+export default suite([
+	{
+		given: "a number with value 16 and a radix of 16",
+		must: "return the string '10'",
+		received: numberToString(16)(16),
+		wanted: "10"
+	},
+	{
+		given: "a number with value 10 and a radix of 10",
+		must: "return the string '10'",
+		received: numberToString(10)(10),
+		wanted: "10"
+	},
+	{
+		given: "a number with value 2 and a radix of 2",
+		must: "return the string '10'",
+		received: numberToString(2)(2),
+		wanted: "10"
+	}
+]);
