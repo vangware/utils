230ef31f7f4d5ae1102a16cf3885243e17d5a36a
Lou Cyx <git@lou.cx>
2023-06-03 14:16:47 -0700

    🐛 fix some type bugs. 📄 typedoc is still broken -\_-

--- src/negate.ts
+++ src/negate.ts
@@ -1,4 +1,4 @@
-import type { Predicate, Single, Unary } from "@vangware/types";
+import type { Filter, Predicate, Single, Unary } from "@vangware/types";
 
 /**
  * Takes a function and returns a copy with negated output.
@@ -15,10 +15,12 @@ import type { Predicate, Single, Unary } from "@vangware/types";
  * @param predicate Function to be negated.
  * @returns Source function with negated output.
  */
-export const negate = <Item, Predicated extends Item>(
-	predicate: Predicate<Item, Predicated>,
+export const negate = <Item, Predicated extends Item = never>(
+	predicate: Single<Predicated> extends Single<never>
+		? Filter<Item>
+		: Predicate<Item, Predicated>,
 ) =>
 	((input: Item) =>
-		!predicate(input)) as Single<Item> extends Single<Predicated>
+		!predicate(input)) as Single<Predicated> extends Single<never>
 		? Unary<Item, boolean>
 		: Predicate<Item, Exclude<Item, Predicated>>;
